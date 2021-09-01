5fa1fcf387e026dbf8cf367f7f887d483f13521c
Hathoom <nianderson@csumb.edu>
2021-08-31 21:30:36 -0700

    Post method done, end of video 3

--- app/src/main/java/com/example/rest_tutorial_project/Comment.java
+++ app/src/main/java/com/example/rest_tutorial_project/Comment.java
@@ -1,2 +1,34 @@
-package com.example.rest_tutorial_project;public class Comment {
-}
+package com.example.rest_tutorial_project;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+        private int postId;
+
+        private int id;
+
+        private String name;
+
+        private String email;
+
+        @SerializedName("body")
+        private String text;
+
+        public int getPostId() {
+            return postId;
+        }
+        public int getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        public String getEmail() {
+            return email;
+        }
+        public String getText() {
+            return text;
+        }
+    }
+
--- app/src/main/java/com/example/rest_tutorial_project/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/rest_tutorial_project/JsonPlaceHolderApi.java
@@ -1,12 +1,54 @@
 package com.example.rest_tutorial_project;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
+    // the _sort &_order, are additional sorting variables.
+    // Integer can use null, while int can't
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    // this to do above, but better
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+
+    //basically same as above, just encoded as an url
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
 }
